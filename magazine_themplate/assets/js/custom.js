7c0975e03221e03e94ddb9e26d2131ad1ec41fec
Nobi Nawas <39197033+nobinawas@users.noreply.github.com>
2019-10-30 02:34:25 -0700

    Add files via upload

---
+++ magazine_themplate/assets/js/custom.js
@@ -0,0 +1,37 @@
+jQuery(document).ready(function() {
+    $('ul.nav li.dropdown').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
+    }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
+    });
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 300) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+    //Click event to scroll to top
+    $('.scrollToTop').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+});
+
+wow = new WOW({
+    animateClass: 'animated',
+    offset: 100
+});
+wow.init();
+
+<!-- Preloader -->
+jQuery(window).load(function() { // makes sure the whole site is loaded
+    $('#status').fadeOut(); // will first fade out the loading animation
+    $('#preloader').delay(700).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(700).css({
+        'overflow': 'visible'
+    });
+})
